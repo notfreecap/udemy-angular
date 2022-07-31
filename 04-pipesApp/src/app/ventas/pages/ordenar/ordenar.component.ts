0273d28d67d301eb11a6e1397a3fe105cb4d5fe5
notfreecap <andresrenew@gmail.com>
2022-07-31 09:30:41 -0500

    pipesApp

--- 04-pipesApp/src/app/ventas/pages/ordenar/ordenar.component.ts
+++ 04-pipesApp/src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -8,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdenarComponent implements OnInit {
 
+  enMayusculas: boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  cambiarPipe(){
+    this.enMayusculas = !this.enMayusculas;
   }
 
 }
